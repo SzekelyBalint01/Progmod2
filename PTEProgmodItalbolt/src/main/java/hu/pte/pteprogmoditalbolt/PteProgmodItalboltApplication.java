9e96e7f575f53077f52397a9c6dd08885bfbeb3d
szekelybalint0101 <szekelybalint0101@gmail.com>
2023-01-05 18:43:31 +0100

    War package added

--- PTEProgmodItalbolt/src/main/java/hu/pte/pteprogmoditalbolt/PteProgmodItalboltApplication.java
+++ PTEProgmodItalbolt/src/main/java/hu/pte/pteprogmoditalbolt/PteProgmodItalboltApplication.java
@@ -2,9 +2,16 @@ package hu.pte.pteprogmoditalbolt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class PteProgmodItalboltApplication {
+
+
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PteProgmodItalboltApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PteProgmodItalboltApplication.class, args);
